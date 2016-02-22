5a7e5dd66cff21fac147c8767c6b15816c2fedd1
xiaopan1991 <xiaopan126@126.com>
2016-02-22 13:50:37 +0800

    222

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuController : MonoBehaviour {
+
+	public SkinnedMeshRenderer[] bodyArray;
+
+	public Color purple;
+	public SkinnedMeshRenderer headRenderer;
+	public Mesh[] headMeshArray;
+	private int headMeshIndex = 0;
+
+	public SkinnedMeshRenderer handRenderer;
+	public Mesh[] handMeshArray;
+	private int handMeshIndex = 0;
+
+
+	public void OnHeadMeshNext()
+	{
+		headMeshIndex++;
+		headMeshIndex %= headMeshArray.Length;
+		headRenderer.sharedMesh = headMeshArray[headMeshIndex];
+	}
+	public void OnHandMeshNext()
+	{
+		handMeshIndex++;
+		handMeshIndex %= handMeshArray.Length;
+		handRenderer.sharedMesh = handMeshArray[handMeshIndex];
+	}
+	public void OnChangeColorBlue()
+	{
+		OnChangeColor(Color.blue);
+	}
+	public void OnChangeColorCyan()
+	{
+		OnChangeColor(Color.cyan);
+	}
+	public void OnChangeColorGreen()
+	{
+		OnChangeColor(Color.green);
+	}
+	public void OnChangeColorPurple()
+	{
+		OnChangeColor(purple);
+	}
+	public void OnChangeColorRed()
+	{
+		OnChangeColor(Color.red);
+	}
+	public void OnPlay()
+	{
+		
+	}
+
+	void OnChangeColor(Color c)
+	{
+		foreach(SkinnedMeshRenderer renderer in bodyArray)
+		{
+			renderer.material.color = c;
+		}
+	}
+}
